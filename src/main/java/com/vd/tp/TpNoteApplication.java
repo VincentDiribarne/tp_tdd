d7aa9a58574dd9c35c7ecc773f401be877e8a86c
Vincent DIRIBARNE <vdiribarnepro@gmail.com>
2025-02-26 16:00:25 +0100

    Init project

---
+++ src/main/java/com/vd/tp/TpNoteApplication.java
@@ -0,0 +1,13 @@
+package com.vd.tp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpNoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpNoteApplication.class, args);
+    }
+
+}
