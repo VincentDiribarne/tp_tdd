90791a35d2bfde50774f7cbaed2baf946b562699
Vincent DIRIBARNE <vdiribarnepro@gmail.com>
2025-02-27 14:23:35 +0100

    Second test ISBN

--- src/main/java/com/vd/tp/validator/ISBNValidator.java
+++ src/main/java/com/vd/tp/validator/ISBNValidator.java
@@ -2,6 +2,22 @@ package com.vd.tp.validator;
 
 public class ISBNValidator {
     public boolean validate(String isbn) {
-        return true;
+        int sum = 0;
+
+        if (isbn.contains("-")) {
+            isbn = isbn.replace("-", "");
+        }
+
+        for (int i = 0; i < isbn.length(); i++) {
+            char c = isbn.charAt(i);
+
+            if (i % 2 == 0) {
+                sum += Character.getNumericValue(c);
+            } else {
+                sum += Character.getNumericValue(c) * 3;
+            }
+        }
+
+        return sum % 10 == 0;
     }
 }
--- src/test/java/com/vd/tp/validator/ISBNValidatorTest.java
+++ src/test/java/com/vd/tp/validator/ISBNValidatorTest.java
@@ -2,6 +2,7 @@ package com.vd.tp.validator;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ISBNValidatorTest {
@@ -12,5 +13,32 @@ public class ISBNValidatorTest {
         boolean result = isbnValidator.validate("978-1917067287");
 
         assertTrue(result);
+    }
+
+    @Test
+    public void shouldIsbnValidWithoutDash() {
+        ISBNValidator isbnValidator = new ISBNValidator();
+
+        boolean result = isbnValidator.validate("978-1917067287");
+
+        assertTrue(result);
+    }
+
+    @Test
+    public void shouldIsbnInvalid() {
+        ISBNValidator isbnValidator = new ISBNValidator();
+
+        boolean result = isbnValidator.validate("9781917067288");
+
+        assertFalse(result);
+    }
+
+    @Test
+    public void shouldIsbnInvalidWithoutDash() {
+        ISBNValidator isbnValidator = new ISBNValidator();
+
+        boolean result = isbnValidator.validate("9781917067288");
+
+        assertFalse(result);
     }
 }
