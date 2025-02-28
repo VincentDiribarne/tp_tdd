89c1b6a0a7a494701599d004b393622649fe05f5
Vincent DIRIBARNE <vdiribarnepro@gmail.com>
2025-02-28 14:16:03 +0100

    Test without Data

--- src/main/java/com/vd/tp/service/MemberService.java
+++ src/main/java/com/vd/tp/service/MemberService.java
@@ -1,5 +1,6 @@
 package com.vd.tp.service;
 
+import com.vd.tp.exception.service.MissingFieldsException;
 import com.vd.tp.model.Member;
 import com.vd.tp.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
@@ -11,6 +12,10 @@ public class MemberService {
     private final MemberRepository repository;
 
     public Member addMember(Member member) {
+        if (member.getMemberCode() == null) throw new MissingFieldsException("Member code is required");
+
+        if (member.getEmail() == null) throw new MissingFieldsException("Email is required");
+
         return saveMember(member);
     }
 
